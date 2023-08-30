9a3e2cd531fec5f03cd818066d8a892d83f6d29e
Flávio Félix <fau_felix30@hotmail.com>
2023-08-30 10:56:58 -0300

    Feito a transactions com managed

--- src/services/employee.service.js
+++ src/services/employee.service.js
@@ -25,33 +25,20 @@ const getById = async (id) => {
 };
 
 const insert = async ({ firstName, lastName, age, city, street, number }) => {
-  const t = await sequelize.transaction();
+  const result = await sequelize.transaction(async (t) => {
+    const employee = await Employee.create({
+      firstName, lastName, age
+    }, { transaction: t });
 
-  try {
-    // Depois executamos as operações
-    const employee = await Employee.create(
-      { firstName, lastName, age },
-      { transaction: t },
-    );
+    await Address.create({
+      city, street, number, employeeId: employee.id
+    }, { transaction: t });
+  });
 
-    await Address.create(
-      { city, street, number, employeeId: employee.id },
-      { transaction: t},
-    );
-
-    // Com isso, podemos finalizar a transação usando a função `commit`.
-
-    await t.commit();
-    return employee;
-  } catch (e) {
-    // Se entrou nesse bloco é porque alguma operação falhou.
-    // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
-    await t.rollback();
-    console.log(e);
-    throw e; // Jogamos o erro para a controller tratar
-
-    
-  }
+  return result;
+  // Se chegou até aqui é porque as operações foram concluídas com sucesso,
+  // não sendo necessário finalizar a transação manualmente.
+  // `result` terá o resultado da transação, no caso um empregado e o endereço cadastrado
 };
 
 module.exports = {
